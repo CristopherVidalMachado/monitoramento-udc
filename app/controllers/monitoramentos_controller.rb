351e0cd5365168afde4a3469c9f151c6f0f2c269
cristophervidalmachado <cristophervidal3@gmail.com>
2020-10-08 13:20:53 -0300

    Adicionando dados de forgery

--- app/controllers/monitoramentos_controller.rb
+++ app/controllers/monitoramentos_controller.rb
@@ -4,7 +4,7 @@ class MonitoramentosController < ApplicationController
   # GET /monitoramentos
   # GET /monitoramentos.json
   def index
-    @monitoramentos = Monitoramento.all
+    @monitoramentos = Monitoramento.eager_load(:localidade).all
   end
 
   # GET /monitoramentos/1
